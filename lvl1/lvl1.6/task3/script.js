3c7ff6113cf0ff2fb03633a483be2ce946afd08b
supchik1604 <Geo1604@yandex.ru>
2025-10-04 13:19:22 +0300

    lvl1.6

---
+++ lvl1/lvl1.6/task3/script.js
@@ -0,0 +1,10 @@
+let a = [1,-1.-2,3]
+let sum =0
+
+for (let i of a){
+    if(i>0){
+        sum+=i
+    }
+}
+
+console.log(i)
---
+++ lvl1/lvl1.6/task4/script.js
@@ -0,0 +1,10 @@
+let a =[1,2,3,11,-2]
+let sum = 0
+
+for(let i  of a){
+    if(i>0 && i<10){
+        sum += i
+    }
+}
+
+console.log(sum)
