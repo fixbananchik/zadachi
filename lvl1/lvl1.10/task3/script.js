017c58bd8a7bc7ffe662c3ce873325ba43a41715
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2025-10-18 12:44:39 +0300

    fix

--- lvl1/lvl1.10/task3/script.js
+++ lvl1/lvl1.10/task3/script.js
@@ -1,7 +1,8 @@
 let a = [1, 2, 3, 4, 5, 6]
+let c = a.length/2
 
-for(let i; i<=a.length/2;i++){
+for(let i=0; i<c;i++){
     console.log(a.slice(0,2))
-    a.pop(a[0])
-    a.pop(a[0])
+    a.shift()
+    a.shift()
 }
